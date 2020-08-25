b8843cec2382621237b14fa2874ee7c1e1a6b733
Vi Nguyen <vnguyen96@dxc.com>
2020-08-25 13:28:33 +0100

    map current location into source location

--- app/src/main/java/com/example/zotee/EventDetailsActivity.java
+++ app/src/main/java/com/example/zotee/EventDetailsActivity.java
@@ -1,9 +1,13 @@
 package com.example.zotee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +18,20 @@ import android.widget.Toast;
 
 import com.example.zotee.activity.HomeActivity;
 import com.example.zotee.MapFragmentActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class EventDetailsActivity extends AppCompatActivity {
     Button btnSetPath;
-    EditText source;
+   // EditText source;
     EditText des;
+    private FusedLocationProviderClient client;
+    private Location currentLocation;
+    private static final int REQUEST_CODE = 101;
+    private String sSource = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +39,55 @@ public class EventDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.event_details_activity);
 
         btnSetPath = findViewById(R.id.bt_track_path);
-        source = findViewById(R.id.etSource);
+      //  source = findViewById(R.id.etSource);
         des = findViewById(R.id.etDestination);
+
+        //initialize fuse location
+        client = LocationServices.getFusedLocationProviderClient(this);
+        getCurrentLocation();
+
 
         btnSetPath.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //get value from edit text
-                String sSource = source.getText().toString().trim();
+               // sSource = source.getText().toString().trim();
                 String sDes = des.getText().toString().trim();
 
                 //check condition
-                if(sSource.isEmpty() && sDes.isEmpty()) {
+                if(sDes.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Please enter both location!", Toast.LENGTH_SHORT).show();
                 } else {
+                    getCurrentLocation();
                     DisplayTrack(sSource, sDes);
+                }
+            }
+        });
+    }
+
+    private void getCurrentLocation() {
+        //Check permission
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        //initialize task location
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    sSource = currentLocation.getLongitude() + "," + currentLocation.getLatitude();
+
+//                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude()
+//                            + "  " + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+//                    SupportMapFragment supMap = (SupportMapFragment) getSupportFragmentManager()
+//                            .findFragmentById(R.id.mapAPI);
+//                    supMap.getMapAsync(MapFragmentActivity.this);
                 }
             }
         });
