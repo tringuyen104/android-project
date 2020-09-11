a7fb41ec8c3d17239e66f75427915e9a56af32f8
thinhnguyen <thinhnguyen6892@gmail.com>
2020-09-11 15:57:19 +0700

    [Thinh][Func] Invite Deep Link integrate - UI improvement

--- app/src/main/java/com/example/zotee/activity/InviteLinkActivity.java
+++ app/src/main/java/com/example/zotee/activity/InviteLinkActivity.java
@@ -1,9 +1,11 @@
 package com.example.zotee.activity;
 
 
-import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,13 +17,21 @@ public class InviteLinkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_link);
+
+        Intent in = getIntent();
+        Uri data = in.getData();
+        String code = data.getQueryParameter("code");
+        EditText inviteCode = findViewById(R.id.inviteInput);
+        inviteCode.setText(code);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-
+        Button next = findViewById(R.id.getInvited);
+        next.setOnClickListener(view -> {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        });
     }
 }
