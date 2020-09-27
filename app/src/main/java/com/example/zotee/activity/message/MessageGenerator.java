f127a657b97a15c4255f8ea7607075c636083bd5
hungthinh <aktv4pro@gmail.com>
2020-09-27 13:11:51 +0700

    Hot-fix

--- app/src/main/java/com/example/zotee/activity/message/MessageGenerator.java
+++ app/src/main/java/com/example/zotee/activity/message/MessageGenerator.java
@@ -11,7 +11,7 @@ public class MessageGenerator {
 
     public static String genInviteMessage(InvitationEntity invitationEntity) {
         String template = "Bạn được mời đến tham dự một buổi gặp gỡ trên ứng dụng Zotee Meeting: \n"
-                + " Để biết thêm thông tin vui lòng tải ứng dụng Zotee và nhập vào mã dưới đây: %s \n"
+                + " Để biết thêm thông tin vui lòng tải ứng dụng Zotee và nhập vào mã dưới đây:  \n"
                 + " Mã của bạn là: %s \n"
                 + " Hoặc \n"
                 + "Chọn vào đường link tại đây nếu bạn đã cài đặt Zotee: \n"
